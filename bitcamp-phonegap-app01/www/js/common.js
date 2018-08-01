9a8dc48a06ebbd3f067104f9e4b98728710e047e
Baihyelim <tcjfja@gamil.com>
2018-08-01 21:41:09 +0900

    20180801

---
+++ bitcamp-phonegap-app01/www/js/common.js
@@ -0,0 +1,1 @@
+var serverApiAddr ="http://192.168.0.51:8080/bitcamp-web-18";
--- bitcamp-web-18/src/main/webapp/html6/js/common.js
+++ bitcamp-web-18/src/main/webapp/html6/js/common.js
@@ -1,1 +1,2 @@
-var serverApiAddr ="http://loclalhost"
+var serverApiAddr ="http://localhost:8080/bitcamp-web-18";
+//var serverApiAddr ="http://192.168.0.51:8080/bitcamp-web-18";
